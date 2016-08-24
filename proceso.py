0bb8d29817245b77a868c8f8f54ee8b523f6235d
Rafalp190 <leo13361@uvg.edu.gt>
2016-08-24 12:01:28 -0600

    cambios a proceso

--- proceso.py
+++ proceso.py
@@ -1,12 +1,14 @@
+# -*- coding: cp1252 -*-
 #Simulacion de un proceso en un procesador
 #@authors: Rafael Leon 13361, Pablo Lopez 14509
 #@version: 0.1
 #@date: 24/8/2016
-#Test
 import simpy as sp
 import random as rnd
+RANDOM_SEED = 32
 
 class proceso(object):
-	def __init__(self,env)
-		cpu= sp.Resource(env, capacity=1)
-		ram= sp.Container(env, capacity=100)
+	def __init__(self,env,id)
+		self.cpu= sp.Resource(env, capacity=1)
+		self.ram= sp.Container(env, init= 100, capacity=100)
+		self.io = sp.Resource(env, capacity=1)

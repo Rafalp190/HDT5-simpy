41f6fb8aa988e6c008965757c047767546c07b2e
Rafalp190 <leo13361@uvg.edu.gt>
2016-08-24 11:44:24 -0600

    se agrego proceso

---
+++ proceso.py
@@ -0,0 +1,11 @@
+#Simulacion de un proceso en un procesador
+#@authors: Rafael Leon 13361, Pablo Lopez 14509
+#@version: 0.1
+#@date: 24/8/2016
+import simpy as sp
+import random as rnd
+
+class proceso(object):
+	def __init__(self,env)
+		cpu= sp.Resource(env, capacity=1)
+		ram= sp.Container(env, capacity=100)
